2efae7e05866d92dbfdf169744b27d7c0af6122d
Dror Ivry <drorivrygmail.com>
2019-11-01 22:04:08 +0200

    added get status for account

--- backend/src/routes/state.js
+++ backend/src/routes/state.js
@@ -1,14 +1,21 @@
 import express from 'express';
-import Cryptr from 'cryptr';
+import mongoosee from 'mongoose';
 
-import Account from '../dal/Account';
 import SystemState from '../dal/SystemState';
 
 
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-
+    const accountID = req.query['account-id'];
+    SystemState.findOne({AccountID: mongoosee.ObjectId(accountID)}, (error,doc) => {
+        if (error)
+            res.send(500, {error});
+        if(!doc)
+            res.status(404).send('Account Not Found!');
+        else
+            res.send(doc);
+    })
 });
 
 export default router;
