f237c026d447714007235f0ef67e20eb0c359121
Dror Ivry <drorivrygmail.com>
2019-11-01 20:57:43 +0200

    added user valiadtion via pass

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -6,13 +6,13 @@ import Account from '../dal/Account'
 
 
 const router = express.Router();
+const cryptr = new Cryptr('myTotalySecretKey');
 
-/* GET users listing. */
+
 router.post('/', function (req, res, next) {
     const username = req.body.username;
     const password = req.body.password;
 
-    const cryptr = new Cryptr('myTotalySecretKey');
     const encryptedPassword = cryptr.encrypt(password);
 
     const generatedKey = uuidv4();
@@ -20,26 +20,45 @@ router.post('/', function (req, res, next) {
     const encryptedKey = cryptr.encrypt(generatedKey);
     Account.update(
         {
-            UserName:username
+            UserName: username
         },
         {
-            $set:{
-                Username:username,
-                Password:encryptedPassword,
+            $set: {
+                Username: username,
+                Password: encryptedPassword,
                 APIKey: encryptedKey,
                 Active: true,
             }
         },
         {
-            upsert:true
+            upsert: true
         },
-            err => {
-            if(err)
+        err => {
+            if (err)
                 res.send(500, {error});
 
             res.send({key: generatedKey});
         }
     );
+});
+
+router.get('/', function (req, res, next) {
+    const b64auth = (req.headers.authorization || '').split(' ')[1] || '';
+    const [username, password] = new Buffer.from(b64auth, 'base64').toString().split(':');
+
+    Account.findOne({UserName: username}, (error, doc) => {
+        if (error)
+            res.send(500, {error});
+
+        const decryptedPass = cryptr.decrypt(doc.Password);
+
+        if (username && password && username === doc.UserName && password === decryptedPass)
+            res.end('validated');
+        else {
+            res.set('WWW-Authenticate', 'Basic realm="401"');
+            res.status(401).send('Authentication required.');
+        }
+    })
 });
 
 export default router;
