397be37e1c7083b5a339c3f1d6a217be2bcb1b77
Dror Ivry <drorivrygmail.com>
2019-11-08 16:43:25 +0200

    added react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
 import SignIn from './components/Login'
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
 
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-        {SignIn()}
-    </div>
-  );
+export default function App() {
+    return (
+        <Router>
+          <div>
+            <Switch>
+              <Route path="/login">
+                <SignIn />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    );
 }
-
-export default App;
