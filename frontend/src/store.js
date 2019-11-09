3b02cdde8988b3575afd2b2d089fc8d2214c4c5b
Dror Ivry <drorivrygmail.com>
2019-11-09 13:47:44 +0200

    hooked up redux

---
+++ frontend/src/store.js
@@ -0,0 +1,7 @@
+import {createStore} from 'redux';
+import logger from 'redux-logger';
+import reducers from './reducers';
+import {applyMiddleware} from 'redux';
+import promiseMiddleware  from 'redux-promise-middleware';
+
+export default createStore(reducers,{}, applyMiddleware(logger, promiseMiddleware()));
