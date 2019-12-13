b58d15303eb9fe9c828b46b00e0027afd9e5e949
dror ivry <drorivry@gmail.con>
2019-12-13 16:17:06 +0200

    WIP

--- dist/build/precache-manifest.c6f13503cec54c95319609b1bcfafb32.js
+++ dist/build/precache-manifest.c6f13503cec54c95319609b1bcfafb32.js
@@ -1,18 +1,22 @@
-"use strict";
-
-self.__precacheManifest = (self.__precacheManifest || []).concat([{
-  "revision": "e290296e9db7107e47a45590ddea6998",
-  "url": "/index.html"
-}, {
-  "revision": "ebb761bccf775d24d1b3",
-  "url": "/static/css/main.34de6062.chunk.css"
-}, {
-  "revision": "e36276559dbcd89f81d3",
-  "url": "/static/js/2.945ec962.chunk.js"
-}, {
-  "revision": "ebb761bccf775d24d1b3",
-  "url": "/static/js/main.df087793.chunk.js"
-}, {
-  "revision": "9aabb23bcbd2c9e69d36",
-  "url": "/static/js/runtime-main.baeb1881.js"
-}]);
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e290296e9db7107e47a45590ddea6998",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ebb761bccf775d24d1b3",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "e36276559dbcd89f81d3",
+    "url": "/static/js/2.945ec962.chunk.js"
+  },
+  {
+    "revision": "ebb761bccf775d24d1b3",
+    "url": "/static/js/main.df087793.chunk.js"
+  },
+  {
+    "revision": "9aabb23bcbd2c9e69d36",
+    "url": "/static/js/runtime-main.baeb1881.js"
+  }
+]);
--- dist/build/service-worker.js
+++ dist/build/service-worker.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -12,22 +10,30 @@
  * and re-run your build process.
  * See https://goo.gl/2aRDsh
  */
+
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-importScripts("/precache-manifest.c6f13503cec54c95319609b1bcfafb32.js");
-self.addEventListener('message', function (event) {
+
+importScripts(
+  "/precache-manifest.c6f13503cec54c95319609b1bcfafb32.js"
+);
+
+self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
+
 workbox.core.clientsClaim();
+
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-  blacklist: [/^\/_/, /\/[^\/?]+\.[^\/]+$/]
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
